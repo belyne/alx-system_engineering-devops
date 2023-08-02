33938b12001be575bbb588ef3ba4fe294b300687
belyne <nzabonamarie@gmail.com>
2023-08-01 23:06:06 -0700

    Repetition Token #1

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
-def match_school(text)
-  # Regular expression to match "School" in the given text
-  regex = /School/
+def match_repetition_token(text)
+  # Regular expression to match 'htn' or 'hbtn' in the given text
+  regex = /htn|hb+tn/
 
   # Use the `scan` method to find all occurrences of the regex pattern in the text
   matches = text.scan(regex).join
@@ -17,5 +17,5 @@ if ARGV.length != 1
   exit 1
 end
 
-# Call the match_school method with the provided argument
-match_school(ARGV[0])
+# Call the match_repetition_token method with the provided argument
+match_repetition_token(ARGV[0])
