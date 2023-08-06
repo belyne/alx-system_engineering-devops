7b455b1c8c4bceed40a1ab2931e5b3712e65e76d
belyne <nzabonamarie@gmail.com>
2023-08-06 14:17:22 -0700

    Repetition Token #2

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,21 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school(text)
-  # Regular expression to match "School" as a whole word in the given text
-  regex = /\bSchool\b/
-
-  # Use the `scan` method to find all occurrences of the regex pattern in the text
-  matches = text.scan(regex).join
-
-  # Output the matched result
-  puts matches
-end
-
-# Accept one argument from the command line
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <text>"
-  exit 1
-end
-
-# Call the match_school method with the provided argument
-match_school(ARGV[0])
+puts ARGV[0].scan(/hbt+n/).join
