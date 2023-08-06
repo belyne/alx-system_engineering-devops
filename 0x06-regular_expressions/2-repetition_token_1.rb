78ff1794b84e1cc29a6b38f2144758217225d471
belyne <nzabonamarie@gmail.com>
2023-08-06 14:16:01 -0700

    Repetition Token #1

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,21 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_repetition_token(text)
-  # Regular expression to match 'htn' or 'hbtn' in the given text
-  regex = /htn|hb+tn/
-
-  # Use the `scan` method to find all occurrences of the regex pattern in the text
-  matches = text.scan(regex).join
-
-  # Output the matched result
-  puts matches
-end
-
-# Accept one argument from the command line
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <text>"
-  exit 1
-end
-
-# Call the match_repetition_token method with the provided argument
-match_repetition_token(ARGV[0])
+puts ARGV[0].scan(/hb?t?n/).join
