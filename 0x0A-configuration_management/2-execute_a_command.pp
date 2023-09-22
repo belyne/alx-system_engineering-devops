662535f65fb3c901b1acedfe4634da3fcaae968e
belyne <nzabonamarie@gmail.com>
2023-09-22 06:02:55 -0700

    0x0A. Configuration management

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,9 +1,14 @@
-# 2-execute_a_command.pp
+# File: 2-execute_a_command.pp
 # Description: Kill a process named killmenow using exec resource.
+
+file { '/tmp/killmenow':
+  ensure  => file,
+  content => "This file ensures the killmenow process can be terminated.",
+}
 
 exec { 'killmenow':
   command     => 'pkill killmenow',
   path        => '/usr/bin:/bin',
-  subscribe   => File['/tmp/killmenow'], # Ensure this file exists before executing the command
   refreshonly => true,
+  subscribe   => File['/tmp/killmenow'],
 }
