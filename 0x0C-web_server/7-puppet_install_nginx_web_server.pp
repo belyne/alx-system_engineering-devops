0e36a548dcf2726e392554e9a736e3c89ec559de
belyne <nzabonamarie@gmail.com>
2023-09-28 12:07:30 -0700

    web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,107 +1,25 @@
-<<<<<<< HEAD
-# This Puppet manifest installs and configures an Nginx server with a 301 redirect.
+# Script to install nginx using puppet
 
-# Install Nginx package
-package { 'nginx':
-  ensure => 'installed',
+package {'nginx':
+  ensure => 'present',
 }
 
-# Define Nginx configuration
-file { '/etc/nginx/sites-available/redirect_me':
-  ensure  => 'file',
-  content => 'location /redirect_me {
-                  return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-              }',
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+
 }
 
-# Create symbolic link for Nginx configuration
-file { '/etc/nginx/sites-enabled/redirect_me':
-  ensure => 'link',
-  target => '/etc/nginx/sites-available/redirect_me',
+exec {'Hello':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
 }
 
-# Ensure Nginx is listening on port 80
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => 'server {
-                  listen 80 default_server;
-                  listen [::]:80 default_server;
-                  root /var/www/html;
-                  index index.html;
-                  location / {
-                      try_files $uri $uri/ =404;
-                  }
-              }',
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/belyne.tech\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
 }
 
-# Restart Nginx service
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  require   => [File['/etc/nginx/sites-available/default'], File['/etc/nginx/sites-available/redirect_me']],
-  subscribe => [File['/etc/nginx/sites-available/default'], File['/etc/nginx/sites-available/redirect_me']],
-}
-
-# Ensure custom page exists
-file { '/var/www/html/index.html':
-  ensure  => 'file',
-  content => 'Hello World!',
-=======
-# 7-puppet_install_nginx_web_server.pp
-
-# Define class for nginx installation and configuration
-class nginx {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  service { 'nginx':
-    ensure  => running,
-    require => Package['nginx'],
-  }
-
-  file { '/usr/share/nginx/html/index.html':
-    ensure  => file,
-    content => '<html><body>Hello World!</body></html>',
-    require => Package['nginx'],
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => "server {
-      listen 80 default_server;
-      listen [::]:80 default_server;
-
-      root /usr/share/nginx/html;
-      index index.html;
-
-      server_name _;
-
-      location / {
-          try_files $uri $uri/ =404;
-      }
-  }",
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-}
-
-include nginx
-
-# Perform a 301 redirect
-file { '/etc/nginx/sites-available/temp_redirect':
-  ensure  => file,
-  content => "server {
-    listen 80;
-    server_name _;
-    return 301 https://belyne.tch;
-  }",
-  notify => Service['nginx'],
-}
-
-# Enable the temporary redirect
-file { '/etc/nginx/sites-enabled/temp_redirect':
-  ensure => link,
-  target => '/etc/nginx/sites-available/temp_redirect',
->>>>>>> ee8ba18251e54b8bc657ff5eeeaca2f81429d679
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
