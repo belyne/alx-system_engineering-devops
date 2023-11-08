eb74c54e9ffd3c8ef6d6679639a437c66c2a48db
belyne <nzabonamarie@gmail.com>
2023-11-08 20:10:46 +0000

    web_stack_debugging_3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,17 @@
+# Define variables
+$file_to_edit = '/path/to/your/file'
+$line_to_replace = 'phpp'
+$replacement_line = 'php'
+
+# Ensure the file exists
+file { $file_to_edit:
+  ensure => present,
+}
+
+# Use Augeas to replace the specified line
+augeas { 'replace_line':
+  context => "/files${file_to_edit}",
+  changes => "set *[contains(text(), '${line_to_replace}')][last()] '${replacement_line}'",
+  onlyif  => "match *[contains(text(), '${line_to_replace}')] size > 0",
+  require => File[$file_to_edit],
+}
